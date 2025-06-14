617589957f9a50b72be577fc887230803ecf9d8a
priavi0108 <bhartipriyanka982@gmail.com>
2025-06-15 00:33:59 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
+
 onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('🎂 Happy Birthday to the bravest heart I know.🪖 Happy Father’s Day to my eternal protector.Thank you for your service, your love, and your life lessons.
-I’m endlessly proud ').split('')
+    const titles = ('I LOVE U').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
