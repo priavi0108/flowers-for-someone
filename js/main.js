87e81624550afd5631f289f37786fab6a4b393c1
priavi0108 <bhartipriyanka982@gmail.com>
2025-06-15 01:14:59 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Birthday to the bravest heart I know & Happy Father’s Day we are endlessly proud — Priya' ).split('')
+    const titles = (' Happy Birthday to the bravest heart I know & Happy Father’s Day we are endlessly proud — Priya ' ).split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
@@ -11,7 +11,7 @@ onload = () => {
       if (index < titles.length) {
         titleElement.innerHTML += titles[index];
         index++;
-        setTimeout(appendTitle, 200); // 1000ms delay
+        setTimeout(appendTitle, 300); // 1000ms delay
       }
     }
 
