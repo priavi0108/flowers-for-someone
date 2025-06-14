3f5a29b2622174da297b3c90b5c1426ed8d360c3
priavi0108 <bhartipriyanka982@gmail.com>
2025-06-15 01:19:54 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Birthday to the bravest heart I know & Happy Father’s Day we are endlessly proud — Priya' ).split('')
+    const titles = ('Happy Birthday to the bravest heart I know & Happy Father’s Day we are endlessly proud — Priya').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
